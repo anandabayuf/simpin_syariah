2de2b9ecf6276ec2281cd1dffb0c0d0360105952
Ananda Bayu <ananda.bayu.tif418@polban.ac.id>
2021-11-16 12:08:25 +0700

    Update RiwayatPembiayaanScreen.dart

--- lib/views/pembiayaan/screens/RiwayatPembiayaanScreen.dart
+++ lib/views/pembiayaan/screens/RiwayatPembiayaanScreen.dart
@@ -2,15 +2,19 @@ import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:project_simpin_syariah/models/pembiayaan/Pembiayaan.dart';
 import 'package:project_simpin_syariah/views/customwidgets/BottomNav.dart';
 import 'package:project_simpin_syariah/views/customwidgets/CustomText.dart';
 import 'package:project_simpin_syariah/views/customwidgets/ProfileButton.dart';
+import 'package:project_simpin_syariah/views/pembiayaan/screens/AjukanPembiayaanScreen1.dart';
 import 'package:project_simpin_syariah/views/pembiayaan/widgets/ContainerRiwayatPembiayaan.dart';
 
 class RiwayatPembiayaanScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+    final pembiayaan = new Pembiayaan("", "", "", "", "", "", "", DateTime.now(),
+    0, 0, 0, 0, 0, "", "", "", "", "", "");
 
     return Scaffold(
       extendBody: true,
@@ -36,10 +40,6 @@ class RiwayatPembiayaanScreen extends StatelessWidget{
                     alignment: Alignment.centerLeft,
                   ),
                   SizedBox(height: 10.0,),
-                  // SizedBox(
-                  //   height: screenSize.height,
-                  //   child: ContainerRiwayatPembiayaan()
-                  // ),
                   ContainerRiwayatPembiayaan()
                 ],
               ),
@@ -53,6 +53,10 @@ class RiwayatPembiayaanScreen extends StatelessWidget{
         child: FloatingActionButton(
           onPressed: (){
             print("Ajukan Pembiayaan");
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AjukanPembiayaanScreen1(pembiayaan: pembiayaan))
+            );
           },
           elevation: 5.0,
           shape: RoundedRectangleBorder(
