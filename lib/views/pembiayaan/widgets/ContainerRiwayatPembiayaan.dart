a52dc67d95b98268d2bb69167cfdf01be4d213b2
Ananda Bayu <ananda.bayu.tif418@polban.ac.id>
2021-11-20 16:36:55 +0700

    Update ContainerRiwayatPembiayaan.dart
    *add sized box in bottom
    *add custom text if not data.length > 0

--- lib/views/pembiayaan/widgets/ContainerRiwayatPembiayaan.dart
+++ lib/views/pembiayaan/widgets/ContainerRiwayatPembiayaan.dart
@@ -27,126 +27,131 @@ class _ContainerRiwayatPembiayaanState extends State<ContainerRiwayatPembiayaan>
     final item = listDatas;
     final formatCurrency = new NumberFormat.currency(locale: "id_ID", symbol: "Rp", decimalDigits: 0);
 
-    return ListView.builder(
-      itemCount: listDatas.length,
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemBuilder: (BuildContext context, int index){
-        return Container(
-          child: InkWell(
-            onTap: (){
-              print("card ${index}");
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>
-                      DetailRiwayatPembiayaanScreen(idRiwayatPembiayaan: index))
-              );
-            },
-            child: Card(
-              color: Colors.white.withOpacity(0.5),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 5.0,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: CustomText("Nilai Pembiayaan", 15.0, true)
-                    ),
-                    SizedBox(height: 5.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("${formatCurrency.format(item[index].nilaiPembiayaan)}", 15.0, false),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("Tujuan Pembiayaan", 15.0, true),
-                    ),
-                    SizedBox(height: 5.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("${item[index].tujuanPembiayaan}", 15.0, false),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("Jenis Akad", 15.0, true),
-                    ),
-                    SizedBox(height: 5.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("${item[index].jenisAkad}", 15.0, false),
-                    ),
-                    SizedBox(height: 15.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: CustomText("Status Pembiayaan", 15.0, true),
-                    ),
-                    SizedBox(height: 5.0,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                        height: 30.0,
-                        width: 75,
-                        child: Card(
-                          elevation: 5.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          color: item[index].statusPembiayaan == "Lunas" ? HexColor("#00C851") : Colors.orange,
-                          child: Align(
-                            child: CustomText("${item[index].statusPembiayaan}", 15.0, false),
-                            alignment: Alignment.center,
-                          ),
+    return listDatas.length > 0 ? Column(
+      children: [
+        ListView.builder(
+          itemCount: listDatas.length,
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (BuildContext context, int index){
+            return Container(
+              child: InkWell(
+                onTap: (){
+                  print("card ${index}");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>
+                          DetailRiwayatPembiayaanScreen(idRiwayatPembiayaan: index))
+                  );
+                },
+                child: Card(
+                  color: Colors.white.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Align(
+                            alignment: Alignment.centerLeft,
+                            child: CustomText("Nilai Pembiayaan", 15.0, true)
                         ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        width: 150,
-                        child: InkWell(
-                          onTap: (){
-                            print("lihat Detail ${index}");
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>
-                                    DetailRiwayatPembiayaanScreen(idRiwayatPembiayaan: index))
-                            );
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text(
-                                'Lihat Detail',
-                                style: TextStyle(
-                                    fontSize: 15.0,
-                                    color: Colors.white,
-                                    fontFamily: 'Comfortaa'
-                                ),
+                        SizedBox(height: 5.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("${formatCurrency.format(item[index].nilaiPembiayaan)}", 15.0, false),
+                        ),
+                        SizedBox(height: 15.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("Tujuan Pembiayaan", 15.0, true),
+                        ),
+                        SizedBox(height: 5.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("${item[index].tujuanPembiayaan}", 15.0, false),
+                        ),
+                        SizedBox(height: 15.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("Jenis Akad", 15.0, true),
+                        ),
+                        SizedBox(height: 5.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("${item[index].jenisAkad}", 15.0, false),
+                        ),
+                        SizedBox(height: 15.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText("Status Pembiayaan", 15.0, true),
+                        ),
+                        SizedBox(height: 5.0,),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            height: 30.0,
+                            width: 75,
+                            child: Card(
+                              elevation: 5.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
                               ),
-                              SizedBox(width: 5.0,),
-                              Icon(
-                                Icons.arrow_forward_rounded,
-                                size: 32.0,
-                                color: Colors.white,
-                              )
-                            ],
+                              color: item[index].statusPembiayaan == "Lunas" ? HexColor("#00C851") : Colors.orange,
+                              child: Align(
+                                child: CustomText("${item[index].statusPembiayaan}", 15.0, false),
+                                alignment: Alignment.center,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                  ],
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            width: 150,
+                            child: InkWell(
+                              onTap: (){
+                                print("lihat Detail ${index}");
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) =>
+                                        DetailRiwayatPembiayaanScreen(idRiwayatPembiayaan: index))
+                                );
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'Lihat Detail',
+                                    style: TextStyle(
+                                        fontSize: 15.0,
+                                        color: Colors.white,
+                                        fontFamily: 'Comfortaa'
+                                    ),
+                                  ),
+                                  SizedBox(width: 5.0,),
+                                  Icon(
+                                    Icons.arrow_forward_rounded,
+                                    size: 32.0,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        );
-      },
-    );
+            );
+          },
+        ),
+        SizedBox(height: 60.0,)
+      ],
+    ) : CustomText("Belum ada riwayat Investasi", 15.0, true);
   }
 }
