df6c67dabdac9dcf3e69fa2bd5b7faeb1fac949c
Ananda Bayu <ananda.bayu.tif418@polban.ac.id>
2021-11-26 19:40:16 +0700

    Update main.dart
    *menambahkan settingan agar dapat swipe untuk popup
    *menambahkan home yaitu Main()
    *menghapus rute yang tidak diperlukan

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:project_simpin_syariah/views/appversion/screen/AppVersionScreen.dart';
+import 'package:project_simpin_syariah/views/Main.dart';
 import 'package:project_simpin_syariah/views/forgetpassword/screens/EmailVerificationScreen.dart';
 import 'package:project_simpin_syariah/views/forgetpassword/screens/InputEmailScreen.dart';
 import 'package:project_simpin_syariah/views/forgetpassword/screens/MakeNewPassScreen.dart';
 import 'package:project_simpin_syariah/views/home/screen/HomeScreen.dart';
 import 'package:project_simpin_syariah/views/investasi/screens/RiwayatInvestasiScreen.dart';
 import 'package:project_simpin_syariah/views/login/screen/LoginScreen.dart';
-import 'package:project_simpin_syariah/views/pembiayaan/screens/DetailRiwayatPembiayaanScreen.dart';
 import 'package:project_simpin_syariah/views/pembiayaan/screens/RiwayatPembiayaanScreen.dart';
 import 'package:project_simpin_syariah/views/profile/screens/ProfileScreen.dart';
 import 'package:project_simpin_syariah/views/simpanan/screens/RiwayatSimpananScreen.dart';
@@ -21,17 +20,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simpin Syariah',
       theme: ThemeData(
-        fontFamily: 'Comfortaa'
+        fontFamily: 'Comfortaa',
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          }
+        )
       ),
-      initialRoute: '/investasi/riwayat',
+      home: Main(),
+      initialRoute: '/login',
       routes: {
         '/login': (context) => LoginScreen(),
         '/lupa-password': (context) => InputEmailScreen(),
         '/verifikasi-email': (context) => EmailVerificationScreen(),
         '/buat-password-baru': (context) => MakeNewPassScreen(),
-        '/home': (context) => HomeScreen(),
-        '/versi': (context) => AppVersionScreen(),
-        '/profile': (context) => ProfileScreen(),
+        // '/home': (context) => HomeScreen(),
+        // '/profile': (context) => ProfileScreen(),
         '/pembiayaan/riwayat': (context) => RiwayatPembiayaanScreen(),
         '/simpanan/riwayat': (context) => RiwayatSimpananScreen(),
         '/investasi/riwayat': (context) => RiwayatInvestasiScreen(),
